3de4f35b3db77b6146c20c83a6894d7356368ae1
zxh <zxh15166423670@gmail.com>
2025-05-26 23:37:17 +0800

    React 18

---
+++ 前端学习/react/React18/my-react-app/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./Header"
+import Footer from "./Footer"
+import Food from './Food'
+
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <Food/>
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ 前端学习/react/React18/my-react-app/src/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer>
+      <p>&copy; {new Date().getFullYear()} Your website home</p>
+    </footer>
+  )
+}
+
+export default Footer
